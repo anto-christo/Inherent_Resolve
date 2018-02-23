5301de940b8b91d855702a73c54dbf2f60fb36cd
Anto <anto.christo.20@gmail.com>
2018-02-23 16:52:33 +0530

    changed port to 3012

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/control',function(req,res){
     res.sendFile(__dirname+'/control.html');
 });
 
-server.listen(process.env.PORT || 3000,function(){
+server.listen(process.env.PORT || 3012,function(){
     console.log('Listening on '+server.address().port);
 });
 
